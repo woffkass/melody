927d30f6abf5dc211b59fb4955b6e6ed2764f53e
Kasap Vladimir <woffkass@gmail.com>
2021-08-19 17:41:52 +0700

    day 3

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  var currentFloor = 2
+  var floorPath = $('.home-image path')
+  var counterUp = $('.counter-up')
+  var counterDown = $('.counter-down')
+  floorPath.on('mouseover', function () {
+    floorPath.removeClass('current-floor')
+    currentFloor = $(this).attr('data-floor')
+    $('.counter').text(currentFloor)
+  })
+
+  counterUp.on('click', function () {
+    if (currentFloor < 18) {
+      currentFloor++
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      })
+      $('.counter').text(usCurrentFloor)
+      floorPath.removeClass('current-floor')
+      $(`[data-floor=${usCurrentFloor}]`).addClass('current-floor')
+    }
+  })
+
+  counterDown.on('click', function () {
+    if (currentFloor > 2) {
+      currentFloor--
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      })
+      $('.counter').text(usCurrentFloor)
+      floorPath.removeClass('current-floor')
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
+    }
+  })
+})
